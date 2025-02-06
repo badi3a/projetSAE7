08e621154ac7d842bd1b8f8013994ad72ff22e13
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2025-02-06 11:23:15 +0100

    first rest Api

---
+++ backEnd/microservice1/src/main/java/esprit/microservice1/CandidatRestApi.java
@@ -0,0 +1,12 @@
+package esprit.microservice1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CandidatRestApi {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
